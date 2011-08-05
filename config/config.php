402344a95adf4140323ed20ec456663bb9982d3d
Jhon Adrián Cerón <jadrian.ceron@gmail.com>
2011-08-05 14:52:24 -0500

    Configuración de la aplicación
    Signed-off-by: Jhon Adrián Cerón <jadrian.ceron@gmail.com>

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+/** Configuration Variables **/
+
+define ('DEVELOPMENT_ENVIRONMENT',true);
+
+define('DB_NAME', 'sigabu');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_HOST', 'localhost');
+
+define('BASE_PATH','http://localhost/sigabu');
+
+define('PAGINATE_LIMIT', '15');
