d2d0a60d8c61e49041b5fffedea1b3672432c5a3
Jhon Adrián Cerón <jadrian.ceron@gmail.com>
2011-08-06 14:32:52 -0500

    Primera edición del menú de la aplicación
    Signed-off-by: Jhon Adrián Cerón <jadrian.ceron@gmail.com>

---
+++ config/menu_project.php
@@ -0,0 +1,66 @@
+<?php
+
+$menu_project = array(
+
+	"personas" => array(		
+		"ico" => "",
+		"desc" => "",
+		"nivel" => 3, ## nivel mínimo exigido por una acción del controlador				
+		"actions" => array(
+					
+			"index" => array(
+				"text" => "Personas",
+				"nivel" => 3,
+				"showMenu" => true 
+			), /* end index */
+			"nuevo" => array(
+				"text" => "Nuevo",
+				"nivel" => 3,
+				"showMenu" => true	
+			), /* end nuevo*/
+			"editar" => array(
+				"nivel" => 3,
+				"showMenu" => false
+			), /* end editar */
+			"eliminar" => array(
+				"nivel" => 5,
+				"showMenu" => false
+			) /* end eliminar */			
+						
+		) /* end actions */						
+	), /* end personas */
+
+	"actividades" => array(
+		"ico" => "",
+		"desc" => "",
+		"nivel" => 2, ## nivel mínimo exigido por una acción del controlador
+		"actions" => array(
+		
+		
+		
+		) /* end actions */	
+	), /* end actividades */
+	
+	"lugares" => array(
+		"ico" => "",
+		"desc" => "",
+		"nivel" => 3, ## nivel mínimo exigido por una acción del controlador
+		"actions" => array(
+		
+		
+		
+		) /* end actions */
+	), /* end lugares */
+	
+	"usuarios" => array(
+		"ico" => "",
+		"desc" => "",
+		"nivel" => 5, ## nivel mínimo exigido por una acción del controlador
+		"actions" => array(
+		
+		
+		
+		) /* end actions */
+	) /* end usuarios */
+
+);
