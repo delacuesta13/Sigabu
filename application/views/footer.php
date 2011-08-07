06253b88a322efa8d22cbac213f4b3abccbd8712
Jhon Adrián Cerón <jadrian.ceron@gmail.com>
2011-08-06 19:28:59 -0500

    Pie de página de la interfaz
    Signed-off-by: Jhon Adrián Cerón <jadrian.ceron@gmail.com>

---
+++ application/views/footer.php
@@ -0,0 +1,23 @@
+
+						<div> <!-- end inner -->
+					</div> <!-- end content  -->
+				<div> <!-- end block -->
+			
+			</div> <!-- end main -->
+			<div id="sidebar">
+			</div> <!-- end sidebar -->
+
+			<?php 
+			$text_footer = 'Copyright &copy; 2011 Jhon Adrián Cerón Guzmán.';
+			?>
+			
+			<script type="text/javascript">
+			<!--
+			$('#main').append('<div id="footer"><div class="block"><p><?php echo $text_footer?><\/p><\/div><\/div>');
+			//-->
+			</script>
+		
+		</div> <!-- end wrapper -->
+	</div> <!-- end container -->
+</body>
+</html>
