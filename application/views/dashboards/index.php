36dd461282f40c561e8e5a47a264e9f06de7e23c
Jhon Adrián Cerón <jadrian.ceron@gmail.com>
2011-08-08 16:39:16 -0500

    Implementada la lista (gráfica) de controladores y los mensajes que han de mostrarse.
    Signed-off-by: Jhon Adrián Cerón <jadrian.ceron@gmail.com>

--- application/views/dashboards/index.php
+++ application/views/dashboards/index.php
@@ -1,1 +1,38 @@
-<?php
+<?php 
+if(isset($showMensaje) && is_array($showMensaje) && count($showMensaje)!=0){
+	?>
+	<div class="flash">
+		<div class="message <?php echo $showMensaje['tipo']?>">
+			<p><?php echo $showMensaje['mensaje']?></p>
+		</div>
+	</div>
+	<?php 
+}
+?>
+
+<ul class="controllerslist">
+<?php 
+
+/**
+ * Imprimir dashboard de controladores
+ */
+
+$menu_project = $GLOBALS['menu_project'];
+
+foreach ($menu_project as $controlador => $opciones){
+	
+	if($_SESSION['nivel'] >= $opciones['nivel']){		
+		?>
+		<li title="<?php echo $opciones['desc']?>">
+			<?php 
+			echo $html->link($html->includeImg($opciones['ico'], $controlador) . "\n<span>" . strtoupper($controlador) . "</span>", $controlador . '/');
+			?>
+		</li>
+		<?php		
+	}
+	
+}
+
+unset($menu_project, $controlador, $opciones);
+?>
+</ul>
