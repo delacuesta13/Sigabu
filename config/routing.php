1e68b8773103fe2cf3f090f5ba1444faa68b7c7b
Jhon Adrián Cerón <jadrian.ceron@gmail.com>
2011-08-08 17:11:21 -0500

    Globalizar el controlador y la acción por defecto
    Signed-off-by: Jhon Adrián Cerón <jadrian.ceron@gmail.com>

--- config/routing.php
+++ config/routing.php
@@ -3,4 +3,7 @@
 $default['controller'] = 'dashboards';
 $default['action'] = 'index';
 
+$GLOBALS['default_controller'] = $default['controller'];
+$GLOBALS['default_action'] = $default['action'];
+
 require_once 'menu_project.php';
