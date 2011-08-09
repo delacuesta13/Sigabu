5e28399529c4cbe12e933d7369e1347db611acd7
Jhon Adrián Cerón <jadrian.ceron@gmail.com>
2011-08-09 17:30:23 -0500

    Se implementa la 'action' index. Además se definen los tipos de mensajes que se mostrarán, si así se indica.
    Signed-off-by: Jhon Adrián Cerón <jadrian.ceron@gmail.com>

--- application/controllers/dashboardscontroller.php
+++ application/controllers/dashboardscontroller.php
@@ -2,6 +2,11 @@
 
 class DashboardsController extends VanillaController {
 	
+	/**
+	 *
+	 * @author: Jhon Adrián Cerón <jadrian.ceron@gmail.com>
+	 */
+	
 	function beforeAction () {
 		
 		/*
@@ -16,7 +21,50 @@ class DashboardsController extends VanillaController {
 	
 	}
 	
-	function index() {
+	function index($tipo_mensaje = null, $nro_mensaje = null) {
+		
+		/**
+		 * 
+		 * Lista de mensajes del proyecto ...
+		 * @var array
+		 */
+		$mensajes_project = array(
+			"error" => array(
+				"404" => array(
+					"text" => "Oops! Al parecer la página que intentas acceder no está disponible o definitivamente no existe.",
+					"tipo" => "notice" ## tipo de mensaje: noticia, warning, error
+				),
+				"1" => array(
+					"text" => "Vaya! No tienes el permiso necesario para interactuar con la página solicitada.",
+					"tipo" => "warning"
+				)
+			)
+		);
+		
+		## Validar si se recibe un mensaje que debe ser mostrado
+		if((isset($tipo_mensaje, $nro_mensaje)) && (strlen($tipo_mensaje)!=0 && strlen($nro_mensaje)!=0) && (array_key_exists($tipo_mensaje, $mensajes_project) && array_key_exists($nro_mensaje, $mensajes_project[$tipo_mensaje]))){
+			$this->set('showMensaje', array("mensaje" => $mensajes_project[$tipo_mensaje][$nro_mensaje]['text'], "tipo" => $mensajes_project[$tipo_mensaje][$nro_mensaje]['tipo']));			
+		}
+		
+		/*******************************************************/
+		
+		$tag_js = '
+		$(function (){
+			$("ul.controllerslist li[title]").qtip({
+				position: {
+					my: "bottom left", 
+					at: "top right"
+				},
+				style: {
+					classes: "ui-tooltip-dark"
+				}
+			});
+		});
+		';
+		
+		$this->set('makecss', array('jquery.qtip.min'));
+		$this->set('makejs', array('jquery.qtip.min'));
+		$this->set('make_tag_js', $tag_js);
 		
 	}
 	
