9b2b65667c6788cd3d3eed297a5d457a380a0801
Jhon Adrián Cerón <jadrian.ceron@gmail.com>
2011-08-10 21:28:15 -0500

    Vista donde se muestran las personas.
    Signed-off-by: Jhon Adrián Cerón <jadrian.ceron@gmail.com>

---
+++ application/views/personas/index.php
@@ -0,0 +1,21 @@
+<form action="#" method="get" class="form">
+<div class="columns wat-cf">
+	<div class="column left">
+		<label class="label">Mostrar</label>
+		<select id="reg_pag">
+			<option value="10" selected="selected">10</option>
+			<option value="20">20</option>
+			<option value="50">50</option>
+			<option value="100">100</option>
+		</select>
+		registros por página
+	</div>
+	<div class="column right">
+		<label class="label">Buscar</label>
+		<input type="text" id="search" size="45"/>
+	</div>
+</div>
+</form>
+<div id="dynamic" style="padding-top:15px;"> <!-- div donde cargo el ajax -->
+	
+</div> <!-- end dynamic -->
