c7a3a365a38fd2c875db9c270ed43ba6ee08cdc7
Jhon Adrián Cerón <jadrian.ceron@gmail.com>
2011-08-15 20:43:12 -0500

    Implementada vista index. Dos divs para cargar con ajax en éstos, la gestión de actividades.
    Signed-off-by: Jhon Adrián Cerón <jadrian.ceron@gmail.com>

--- application/models/actividad.php
+++ application/models/actividad.php
@@ -2,4 +2,28 @@
 
 class Actividad extends VanillaModel {
 	
+	/**
+	 *
+	 * Eliminar actividades ...
+	 * @param array $datos
+	 */
+	function eliminar($datos){
+	
+		$j = 0; ## número de querys exitosos
+	
+		## construyo las sentencias de eliminación
+		for($i = 0; $i < count($datos); $i++){
+			## valido que id sea número
+			if(preg_match('/^[\d]{1,}$/', $datos[$i])){
+				## query exitoso
+				if($this->query('DELETE FROM actividades WHERE id = \'' . $datos[$i] . '\'')){
+					$j++;
+				}
+			}
+		}
+	
+		return (array('trueQuery' => $j, 'totalQuery' => count($datos)));
+			
+	}
+	
 }
