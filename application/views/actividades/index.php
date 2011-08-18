863462a03cca1836821e394401e74211cf8ba223
Jhon Adrián Cerón <jadrian.ceron@gmail.com>
2011-08-18 15:05:33 -0500

    Implementados algunos cambios.
    Signed-off-by: Jhon Adrián Cerón <jadrian.ceron@gmail.com>

--- application/views/actividades/index.php
+++ application/views/actividades/index.php
@@ -1,10 +1,10 @@
-<div class="group navform wat-cf" style="margin-bottom:2px">
-	<button class="button">
-			<?php echo $html->includeImg('icons/add.png', 'Nuevo')?> Nuevo
-	</button>
+<div class="group navform wat-cf" id="botonera" style="margin-bottom:2px">
+	<a href="<?php echo BASE_PATH . '/' . strtolower($this->_controller) . '/' . 'nuevo'?>">
+		<button class="button" id="btn_nuevo">
+				<?php echo $html->includeImg('icons/add.png', 'Nuevo')?> Nuevo
+		</button>
+	</a>
 </div>
-
-<hr/>
 
 <div id="showMensaje" class="flash" style="display:none;margin-top:15px"></div>
 
