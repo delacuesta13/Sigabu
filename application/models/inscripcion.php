8bcb09ff077fb5bde17c1ef29dc578672b90bee0
Jhon Adrián Cerón <jadrian.ceron@gmail.com>
2011-09-01 14:37:59 -0500

    Implementada función que crea nuevas inscripciones.
    Signed-off-by: Jhon Adrián Cerón <jadrian.ceron@gmail.com>

--- application/models/inscripcion.php
+++ application/models/inscripcion.php
@@ -1,5 +1,25 @@
 <?php
 
+/*
+ * Copyright (c) 2011 Jhon Adrián Cerón <jadrian.ceron@gmail.com>
+ *
+ * This file is part of the Sigabu project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 class Inscripcion extends VanillaModel {
+	
+function nuevo ($data) {
+		$sql = '
+		INSERT INTO inscripciones SET ';
+		foreach ($data as $field => $value){
+			$sql .= $field . ' = \'' . $value . '\', ';
+		}
+		$sql .= 'fecha_inscripcion = NOW(), ';
+		$sql .= 'created_at = NOW()';
+		return $this->query($sql);
+	}	
 	
 }
