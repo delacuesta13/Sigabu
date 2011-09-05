4e65a82572c9d1bb08d81a92253e71c546be6bcc
Jhon Adrián Cerón <jadrian.ceron@gmail.com>
2011-09-05 16:31:09 -0500

    Corregida url de las inscripciones.
    Signed-off-by: Jhon Adrián Cerón <jadrian.ceron@gmail.com>

--- public/js/personas_inscripciones.js
+++ public/js/personas_inscripciones.js
@@ -9,7 +9,7 @@ function load_dataTable_insc (controlador, dni, pag, record, sort, order, search
 	$(function() {
 						
 		$( "#dynamic-" + controlador ).html( info_preload );
-		var url = url_project + "inscripciones/listar_inscripciones_personas/" + dni;
+		var url = url_project + "inscripciones/listar_inscripciones_persona/" + dni;
 		if(pag.length!=0) url += "/pag=" + pag;
 		if(record.length!=0) url += "/record=" + record;
 		if(sort.length!=0) url += "/sort=" + sort;
