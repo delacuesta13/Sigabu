50811aa528aa9044082a0f9277f9613c8d378917
Jhon Adrián Cerón <jadrian.ceron@gmail.com>
2011-09-05 17:22:19 -0500

    Se quitan funciones que no se usarán.
    Signed-off-by: Jhon Adrián Cerón <jadrian.ceron@gmail.com>

--- public/js/personas_inscripciones.js
+++ public/js/personas_inscripciones.js
@@ -25,33 +25,3 @@ function load_dataTable_insc (controlador, dni, pag, record, sort, order, search
 	});
 		
 }
-
-function closeDialog_insc (dialog, id_msj, div){
-	
-	if (dialog.length!=0) {
-		$(function() {
-			$("#dialog-" + dialog).dialog("close");  
-			return false; 						
-		});
-	}
-	customMensaje_insc(id_msj, div);
-		
-}
-
-function customMensaje_insc (id_msj, div) {
-	
-	var mensajes = new Array();
-	mensajes[0] = "Vaya! No tienes el permiso necesario para interactuar con la página solicitada.";
-	mensajes[1] = "Existe un error al cargar la página solicitada.";	
-
-	var msj_dialog = "<div class=\"message notice\"><p>" + mensajes[id_msj] + "</p></div>"; 
-	
-	$(function() {
-		$( "#showMensaje-" + div ).html(msj_dialog);
-		$( "#showMensaje-" + div ).fadeIn("slow");
-		$(".flash").click(function() {$(this).fadeOut("slow", function() { $(this).css("display", "none"); });});
-	});
-	
-	return false;
-
-}
