6ee76d3dd0e9f162939667195f7864f052de064d
Jhon Adrián Cerón <jadrian.ceron@gmail.com>
2011-09-05 15:27:06 -0500

    Agregada variable que valida los cruces de horarios, antes de realizar una nueva inscripción a actividades.
    Signed-off-by: Jhon Adrián Cerón <jadrian.ceron@gmail.com>

--- config/config.php
+++ config/config.php
@@ -21,3 +21,21 @@ define('DB_HOST', 'localhost');
 define('BASE_PATH','http://localhost/sigabu');
 
 define('PAGINATE_LIMIT', '10');
+
+/** Configuración del comportamiento del proyecto **/
+
+/*
+ * INSCRIPCIONES_CRUCEHRS
+ * tipo: boolean
+ * default: false
+ * INSCRIPCIONES_CRUCEHRS (o "inscripción cruce de horarios"), según su valor,
+ * valida si la inscripción de una persona en un curso (programación de una 
+ * actividad en un determinado periodo) se cruza con los horarios de otras
+ * inscripciones (del mismo periodo).
+ * ++ Comportamiento (según su valor) ++
+ * true: valida que no se crucen los horarios de la inscripción a realizar,
+ * con las inscripciones existentes. Si se encuentra que hay cruce de horarios,
+ * no se permite la nueva inscripción.
+ * false: no valida los cruces de los horarios.    
+ */
+define('INSCRIPCIONES_CRUCEHRS', false);
