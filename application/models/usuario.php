b6ed9e937d52fa8dbc1dcfa0efa72a44e49edb62
Jhon Adrián Cerón <jadrian.ceron@gmail.com>
2011-09-10 22:05:36 -0500

    Implementada función que edita una cuenta de usuario.
    Signed-off-by: Jhon Adrián Cerón <jadrian.ceron@gmail.com>

--- application/models/usuario.php
+++ application/models/usuario.php
@@ -21,6 +21,17 @@ class Usuario extends VanillaModel {
 		return $this->query($sql);
 	}
 	
+	function editar ($persona, $data) {
+		$sql = '
+		UPDATE usuarios SET ';
+		foreach ($data as $field => $value) {
+			$sql .= $field . ' = ' . ((strlen($value)==0) ? 'NULL' : ' \'' . $value . '\'') . ', ';
+		}
+		$sql = substr_replace($sql, '', -2);
+		$sql .= ' WHERE persona_dni = \'' . $persona .'\'';
+		return $this->query($sql);
+	}
+	
 	function eliminar($datos){
 	
 		$j = 0; ## número de querys exitosos
@@ -38,6 +49,24 @@ class Usuario extends VanillaModel {
 	
 		return (array('trueQuery' => $j, 'totalQuery' => count($datos)));
 			
+	}
+	
+	function consultar_usuario ($persona) {
+		$sql = '
+		SELECT persona.nombres, 
+       		   persona.apellidos,
+       		   persona.tipo_dni, 
+       		   usuario.username, 
+       		   usuario.email, 
+       		   usuario.rol_id, 
+       		   usuario.estado, 
+       		   usuario.fecha_activacion 
+		FROM   personas	persona, 
+       		   usuarios usuario 
+		WHERE  usuario.persona_dni = \'' . $persona . '\' 
+       		   AND usuario.persona_dni = persona.dni 
+		';		
+		return $this->query($sql);
 	}
 	
 	###########################################
